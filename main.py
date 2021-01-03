1083245257a2a242dd2a5fba192a8f1a25622e7f
Annmayn <ntvirus333@gmail.com>
2021-01-03 20:17:47 +0545

    add support for command line args

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
-
-from excel.parser import ExcelParser
+import sys
+from html2excel.excel.parser import ExcelParser
 
 # SAMPLE CODE
-file_path = os.path.join(os.getcwd(), "tmp", "html_file.html")
-save_path = os.path.join(os.getcwd(), "tmp", "trial.xlsx")
-parser = ExcelParser(file_path)
-parser.to_excel(save_path)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+        save_path = sys.argv[2]
+    # file_path = os.path.join(os.getcwd(), "tmp", "html_file.html")
+    # save_path = os.path.join(os.getcwd(), "tmp", "trial.xlsx")
+
+    parser = ExcelParser(file_path)
+    parser.to_excel(save_path)
 
 # TODO: handle exceptions
