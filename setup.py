72149f7e4c7324b38b3ca7006791f809ce40739e
Annmayn <ntvirus333@gmail.com>
2021-01-05 19:58:19 +0545

    update setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    readme = f.read()
+
 setup(
     name = "html2excel",
     version = "0.0.1",
     author = "Neema Tsering",
     author_email = "ntvirus333@gmail.com",
     description = ("Convert HTML Table to Excel file"),
+    long_description = readme,
+    long_description_content_type = "text/markdown",
     install_requires = ['bs4', 'openpyxl'],
     license = "MIT",
     packages = ["html2excel"],
