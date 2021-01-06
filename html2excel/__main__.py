0cb8211b49ae884b67ac58ae2f1af82bd7f66f6d
Annmayn <ntvirus333@gmail.com>
2021-01-06 20:21:00 +0545

    patch code for commandline use

---
+++ html2excel/__main__.py
@@ -0,0 +1,15 @@
+import sys
+from html2excel import ExcelParser
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        file_path = sys.argv[1]
+        save_path = sys.argv[2]
+        parser = ExcelParser(file_path)
+        parser.to_excel(save_path)
+    else:
+        # First argument for file name, we'll ignore that
+        print("Expected 2 arguments. Got {num}".format(num=len(sys.argv)-1))
+
+# TODO: handle exceptions
