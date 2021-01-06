33d0cf1536af122d5ce2b4d792445e2dd4f21721
Annmayn <ntvirus333@gmail.com>
2021-01-06 20:21:14 +0545

    upgrade version and add entrypoint

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name = "html2excel",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Neema Tsering",
     author_email = "ntvirus333@gmail.com",
     description = ("Convert HTML Table to Excel file"),
@@ -14,4 +14,9 @@ setup(
     install_requires = ['bs4', 'openpyxl'],
     license = "MIT",
     packages = ["html2excel"],
+    entry_points = {
+        "console_scripts": [
+            "html2excel = __main__:main"
+        ]
+    }
 )
