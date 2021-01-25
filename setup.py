9e9bf7a8b0491d515524a34087cf78d546906602
Annmayn <ntvirus333@gmail.com>
2021-01-25 20:49:51 +0545

    update gitignore, setup

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open('README.md', 'r') as f:
     readme = f.read()
 
 setup(
-    name = "html2excel",
-    version = "0.0.4",
-    author = "Neema Tsering",
-    author_email = "ntvirus333@gmail.com",
-    description = ("Convert HTML Table to Excel file"),
-    long_description = readme,
-    long_description_content_type = "text/markdown",
-    install_requires = ['bs4', 'openpyxl'],
-    license = "MIT",
-    packages = find_packages(),
-    url = "https://github.com/Annmayn/html2excel",
+    name="html2excel",
+    version="0.0.5",
+    author="Neema Tsering",
+    author_email="ntvirus333@gmail.com",
+    description=("Convert HTML Table to Excel file"),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=['bs4', 'openpyxl', 'html5lib'],
+    license="MIT",
+    packages=find_packages(),
+    url="https://github.com/Annmayn/html2excel",
 )
