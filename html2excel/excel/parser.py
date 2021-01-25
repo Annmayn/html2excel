8b23fe238205ec576af0d647c91237111bc1c873
Annmayn <ntvirus333@gmail.com>
2021-01-25 20:44:25 +0545

    add consideration for multiple tables

--- html2excel/excel/parser.py
+++ html2excel/excel/parser.py
@@ -8,18 +8,27 @@ class ExcelParser(Parser):
 
     def load_workbook(self):
         data = self._read_file()
-        soup = BeautifulSoup(data, features='html.parser')
-        table_data = soup.table
-        if table_data is None:
+        soup = BeautifulSoup(data, features='html5lib')
+
+        all_data = soup.html.body.find_all(recursive=False)
+        if all_data is None:
             raise Exception("No table found")
-        data_rows = self._get_row(table_data, ["tr"])
-        for i, row in enumerate(data_rows, 1):
-            columns = self._get_row(row, ["th", "td"])
-            next_j = 1
-            for j, col in enumerate(columns, 1):
-                j = next_j
-                next_j, col_data = self._pre_validate_and_format(i, j, col)
-                self._write_cell(i, j, col_data)
+        i, offset = 0, 0
+        for each in all_data:
+            if each.name == 'br':
+                offset += 1
+            elif each.name == 'table':
+                data_rows = self._get_row(each, ["tr"])
+                for i, row in enumerate(data_rows, 1):
+                    i += offset
+                    columns = self._get_row(row, ["th", "td"])
+                    next_j = 1
+                    for j, col in enumerate(columns, 1):
+                        j = next_j
+                        next_j, col_data = self._pre_validate_and_format(
+                            i, j, col)
+                        self._write_cell(i, j, col_data)
+                offset += i
 
     def to_excel(self, save_file_path: str, ignore_merged_row: bool = True) -> bool:
         # TODO: handle case when rows are merged
